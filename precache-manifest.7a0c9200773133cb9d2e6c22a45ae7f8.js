15229e4a2e325c2c3dd664833b8c81fe78124b03
gamingexpx12 <gamingexpx12@gmail.com>
2019-09-07 18:07:47 +0200

    Updates

---
+++ precache-manifest.7a0c9200773133cb9d2e6c22a45ae7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6fced789d9497f415bdcf47b6a2bee",
+    "url": "/mediawatch/index.html"
+  },
+  {
+    "revision": "d95dc7c5a7b50cb94ccc",
+    "url": "/mediawatch/static/css/main.22e5f379.chunk.css"
+  },
+  {
+    "revision": "d93b8f17bb2ec67c7abd",
+    "url": "/mediawatch/static/js/2.51c0a69d.chunk.js"
+  },
+  {
+    "revision": "d95dc7c5a7b50cb94ccc",
+    "url": "/mediawatch/static/js/main.dfe735d8.chunk.js"
+  },
+  {
+    "revision": "be5d32d0a8e777426fac",
+    "url": "/mediawatch/static/js/runtime~main.e04b0104.js"
+  }
+]);
